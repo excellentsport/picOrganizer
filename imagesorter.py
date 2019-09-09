53f74a3c8c65eb905e50d4dcfe22907ad9dce9b2
George Beckham <35553484+excellentsport@users.noreply.github.com>
2019-09-09 09:35:21 -0700

    forgot to add deleted imagesorter.py file. Added code to loop through image files

--- imagesorter.py
+++
@@ -1,7 +0,0 @@
-import os
-from PIL import Image
-
-def get_date_taken(path):
-    return Image.open(path)._getexif()[36867]
-
-print(get_date_taken("test.jpg"))
--- picOrganizer.py
+++ picOrganizer.py
@@ -4,15 +4,20 @@ from PIL import Image
 def get_date_taken(path):
     return Image.open(path)._getexif()[36867]
 
+def sortfile(filename,yeartaken):
+    #TODO write this function
+
 filepath = os.getcwd()
 
 for file in os.listdir(filepath):
-     print(file)
-
-#TODO: Find all image files in top level folder
-
-#TODO: Parse the strange filenames, changing them if need be
-
-#TODO: Read image metadata, extract year
-
-#TODO: Move file based on year
+    if file.endswith(".png") or file.endswith(".jpeg") or file.endswith(".jpg"):
+        fullpath = filepath + "\\" + file
+        print(fullpath)
+        
+        try:
+            fulldate = get_date_taken(file)
+            year = fulldate[0:4]
+            sortfile(file,year)
+        except:
+            year = None
+            sortfile(file,year)
