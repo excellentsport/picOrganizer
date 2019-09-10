9ce79f9e65c919b02f24f0ac5b15a5e80b5eb7c9
George Beckham <35553484+excellentsport@users.noreply.github.com>
2019-09-10 11:48:36 -0700

    fixing code for videos and setting up regex search for year

--- picOrganizer.py
+++ picOrganizer.py
@@ -1,4 +1,4 @@
-import os
+import os, time, re
 from PIL import Image
 
 def get_date_taken(path):
@@ -8,20 +8,45 @@ def sortfile(filename,yeartaken):
     if not os.path.exists(yeartaken):
         os.makedirs(yeartaken)
     os.rename(filename,yeartaken+'\\'+filename)
+    print(filename + ' moved.')
 
-filetypes = ('.png', '.jpeg', '.jpg', '.gif', '.mov', 'mp4')
-    
-filepath = os.getcwd()
+imagetypes = ('.png', '.jpeg', '.jpg', '.gif')
+videotypes = ('.mov', 'mp4')
 
+yearRegex = re.compile(r'20(\d){2}')
+
+#filepath = os.getcwd()
+filepath = os.path.expanduser('~') + '\\Dropbox\\Camera Uploads'
+print(filepath)
+
+#process images
 for file in os.listdir(filepath):
-    if file.endswith(filetypes):
-        fullpath = filepath + "\\" + file
-        print(fullpath)
-        
+    if file.endswith(imagetypes):
+        fullpath = filepath + '\\' + file  
         try:
             fulldate = get_date_taken(file)
-            year = fulldate[0:4]
+            year = fulldate[0:4] #standard output date is year first.
             sortfile(file,year)
         except:
-            year = "No Year"
-            sortfile(file,year)
+            print('Date taken not found in exif data for ' + file + '.')
+            regexMatch = yearRegex.search(file)
+            year = regexMatch.group()
+            if year is None:
+                print('Year not found in filename for ' + file + ', file not moved.')
+            else:
+                sortfile(file,year)
+
+#process videos
+for file in os.lisdir(filepath):
+    if file.endwith(videotypes):
+        fullpath = filepath + "\\" + file
+
+        try:
+            regexMatch = yearRegex.search(file)
+            year = regexMatch.group()
+            if year is None:
+                print('Year not found in filename for ' + file + ', file not moved.')
+            else:
+                sortfile(file,year)
+        except:
+            print('Unable to sort ' + file + '.')
