1e24ae8e1ca0123fd6a63e1c94547a2a2fc6c4e3
George Beckham <excellentsport@users.noreply.github.com>
2019-09-11 00:47:38 -0700

    wrapped main body of code in main function

--- picOrganizer.py
+++ picOrganizer.py
@@ -1,8 +1,7 @@
 import os, time, re
 from PIL import Image
 
-# TODO: add argparse.ArgumentParser to designate folder to process. Consider option --dry-run 
-# TODO: wrap main body if code in main function 
+# TODO: add argparse.ArgumentParser to designate folder to process. Consider option --dry-run  
 # TODO: run this through linter
 
 # get the year 
@@ -31,32 +30,37 @@ file_path = os.getcwd()
 #file_path = os.path.join(os.path.expanduser('~'), "Dropbox", "Camera Uploads")  # os.path.expanduser('~') gets the path for the user's home folder.
 #os.chdir(file_path)
 
-#Process images
-for file_name in os.listdir(file_path):
-    if file_name.endswith(image_types):  # 'endswith() can be fed a tuple.
-        try:  # Try to get the 'date taken' data from an image file, if it doesn't have this info, it throws an exception.
-            year = year_taken(file_name)
-            sort_file(file_name, year)
-        except:  # When "date taken" isn't found in exif data, get the year from the file name
-            print('Date taken not found in exif data for ' + file_name + '.')
-            regex_match = year_regex.search(file_name)
-            
-            if regex_match is None:  # if you can't find a year in the file name, leave it alone to be manually sorted.
-                print('Year not found in file name for ' + file_name + ', file not moved.')
-            else:
-                year = regex_match.group()
-                sort_file(file_name, year)
+def main():
+	#Process images
+	for file_name in os.listdir(file_path):
+	    if file_name.endswith(image_types):  # 'endswith() can be fed a tuple.
+	        try:  # Try to get the 'date taken' data from an image file, if it doesn't have this info, it throws an exception.
+	            year = year_taken(file_name)
+	            sort_file(file_name, year)
+	        except:  # When "date taken" isn't found in exif data, get the year from the file name
+	            print('Date taken not found in exif data for ' + file_name + '.')
+	            regex_match = year_regex.search(file_name)
+	            
+	            if regex_match is None:  # if you can't find a year in the file name, leave it alone to be manually sorted.
+	                print('Year not found in file name for ' + file_name + ', file not moved.')
+	            else:
+	                year = regex_match.group()
+	                sort_file(file_name, year)
+	
+	#Process videos
+	for file_name in os.listdir(file_path):
+	    if file_name.endswith(video_types):
+	
+	        try:  # There isn't exif data in videos, so do a regex search for the year in the file name
+	            regex_match = year_regex.search(file_name)
+	            if regex_match is None:  # If a year isn't found, then leave the file there to be manually sorted.
+	                print('Year not found in file name for ' + file_name + ', file not moved.')
+	            else: 
+	                year = regex_match.group()
+	                sort_file(file_name, year)
+	        except:
+	            print('Unable to sort ' + file_name + '.')
+	            
 
-#Process videos
-for file_name in os.listdir(file_path):
-    if file_name.endswith(video_types):
-
-        try:  # There isn't exif data in videos, so do a regex search for the year in the file name
-            regex_match = year_regex.search(file_name)
-            if regex_match is None:  # If a year isn't found, then leave the file there to be manually sorted.
-                print('Year not found in file name for ' + file_name + ', file not moved.')
-            else: 
-                year = regex_match.group()
-                sort_file(file_name, year)
-        except:
-            print('Unable to sort ' + file_name + '.')
+if __name__== "__main__":
+  main()
