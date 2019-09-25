95098019938d3c1e1583a9de9b0191903adc8315
George Beckham <35553484+excellentsport@users.noreply.github.com>
2019-09-24 22:48:36 -0700

    better variable naming

--- picOrganizer.py
+++ picOrganizer.py
@@ -7,21 +7,21 @@ from PIL import Image
 
 # get the year
 def year_taken(path):
-	date_taken = Image.open(path)._getexif()[36867]
-	return date_taken[0:4]
+    date_taken = Image.open(path)._getexif()[36867]
+    return date_taken[0:4]
 
 
 # using the year data from the image, check if a folder exists.
 # If not, then create it. Move the file to the folder.
-def sort_file(file_name, year_taken):
-	if not os.path.exists(year_taken):
-		os.makedirs(year_taken)
-	os.rename(file_name, year_taken + '\\' + file_name)
-	print(file_name + ' moved.')
+def sort_file(file_name, year_photo_taken):
+    if not os.path.exists(year_photo_taken):
+        os.makedirs(year_photo_taken)
+    os.rename(file_name, year_photo_taken + '\\' + file_name)
+    print(file_name + ' moved.')
 
 
 # define what image and video types to search for
-image_types = ('.png', '.jpeg', '.jpg', '.gif')
+IMAGE_TYPES = ('.png', '.jpeg', '.jpg', '.gif')
 video_types = ('.mov', 'mp4')
 
 # Search for 4 digit string starting with '20'
@@ -38,41 +38,41 @@ file_path = os.getcwd()
 
 def main():
 
-	# Process images
-	for file_name in os.listdir(file_path):
-		if file_name.endswith(image_types):  # 'endswith() can be fed a tuple.
-			try:
-				# Try to get the 'date taken' image, lack of info throws an exception.
-				year = year_taken(file_name)
-				sort_file(file_name, year)
-			except:
-				# If 'date taken' not in exif data, get the year from the file name
-				print('Date taken not found in exif data for ' + file_name + '.')
-				regex_match = year_regex.search(file_name)
+    # Process images
+    for file_name in os.listdir(file_path):
+        if file_name.endswith(IMAGE_TYPES):  # 'endswith() can be fed a tuple.
+            try:
+                # Try to get the 'date taken' image, lack of info throws an exception.
+                year = year_taken(file_name)
+                sort_file(file_name, year)
+            except:
+                # If 'date taken' not in exif data, get the year from the file name
+                print('Date taken not found in exif data for ' + file_name + '.')
+                regex_match = year_regex.search(file_name)
 
-				if regex_match is None:
-					# if you can't find year in the file name, leave it alone.
-					print('Year not found in filename for ' + file_name + ', file not moved.')
-				else:
-					year = regex_match.group()
-					sort_file(file_name, year)
+                if regex_match is None:
+                    # if you can't find year in the file name, leave it alone.
+                    print('Year not found in filename for ' + file_name + ', file not moved.')
+                else:
+                    year = regex_match.group()
+                    sort_file(file_name, year)
 
-	# Process videos
-	for file_name in os.listdir(file_path):
-		if file_name.endswith(video_types):
+    # Process videos
+    for file_name in os.listdir(file_path):
+        if file_name.endswith(video_types):
 
-			try:
-				# No exif data in videos - do a regex search for year in the file name
-				regex_match = year_regex.search(file_name)
-				if regex_match is None:
-					# If a year isn't found, then leave the file there to be manually sorted.
-					print('Year not found in filename for ' + file_name + ', file not moved.')
-				else:
-					year = regex_match.group()
-					sort_file(file_name, year)
-			except:
-				print('Unable to sort ' + file_name + '.')
+            try:
+                # No exif data in videos - do a regex search for year in the file name
+                regex_match = year_regex.search(file_name)
+                if regex_match is None:
+                    # If a year isn't found, then leave the file there to be manually sorted.
+                    print('Year not found in filename for ' + file_name + ', file not moved.')
+                else:
+                    year = regex_match.group()
+                    sort_file(file_name, year)
+            except:
+                print('Unable to sort ' + file_name + '.')
 
 
 if __name__ == "__main__":
-	main()
+    main()
